cc8bc30de80a9423e17b4f1011b82d7efe58e8ae
zeke <ezekiel.ravidat@gmail.com>
2024-09-25 13:10:41 +0200

    display article

---
+++ src/slices/ArticleContent/index.js
@@ -0,0 +1,30 @@
+/**
+* @typedef {import("@prismicio/client").Content.ArticleContentSlice} ArticleContentSlice
+* @typedef {import("@prismicio/react").SliceComponentProps<ArticleContentSlice>} ArticleContentProps
+* @param {ArticleContentProps}
+*/
+
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText } from "@prismicio/react";
+
+const ArticleContent = ({ slice }) => {
+  return (
+    <section
+    data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="article"
+    >
+      <PrismicNextImage field={slice.primary.article_image} className="article-image" />
+      <div className="article-text">
+        <span>{slice.primary.article_title}</span>
+        <PrismicRichText field={slice.primary.article_description} />
+        <span>{slice.primary.article_date}</span>
+      </div>
+    </section>
+  );
+};
+
+export default ArticleContent;
+
+
+
